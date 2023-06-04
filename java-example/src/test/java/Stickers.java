a09466630041ff53603a77235891025552562878
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2023-06-05 00:47:49 +0300

    [x] Задание 7. Сделайте сценарий, проверяющий наличие стикеров у товаров

--- java-example/src/test/java/Stickers.java
+++ java-example/src/test/java/Stickers.java
@@ -1,9 +1,20 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class Stickers extends TestBase {
-  /*Задание 7.
-
-  Сделайте сценарий, проверяющий наличие стикеров у всех товаров в учебном приложении litecart на главной странице.
-  Стикеры -- это полоски в левом верхнем углу изображения товара, на которых написано New или Sale или что-нибудь другое.
-  Сценарий должен проверять, что у каждого товара имеется ровно один стикер.
-
-  */
+  @Test
+  public void stickersTest() {
+    driver.navigate().to("http://localhost/litecart/");//открыть  litecart на главной странице
+    List<WebElement> products = driver.findElements(By.className("image-wrapper"));//получить список всех товаров с главной страницы
+    for(WebElement product : products){
+      List<WebElement> stickers = product.findElements(By.xpath("./div[contains(@class,'sticker')]"));//на каждом товаре найти стикеры, неважно, что на них написано
+      assertEquals(1, stickers.size());//проверить, что у каждого товара имеется ровно один стикер
+    }
+  }
 }
