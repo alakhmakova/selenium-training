09b072fb5e99cb3516bb2713136eacd71809eea5
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2023-06-06 01:30:43 +0300

    [x] Задание 9. Проверить сортировку геозон на странице геозон

--- java-example/src/test/java/GeoZonesTest.java
+++ java-example/src/test/java/GeoZonesTest.java
@@ -1,16 +1,50 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class GeoZonesTest extends TestBase{
 
   @Test
   public void geoZonesTest() {
 
-  /*
-  [x] Задание 9. Проверить сортировку геозон на странице геозон
-Сделайте сценарии, который на странице http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones заходит в каждую из стран и проверяет, что зоны расположены в алфавитном порядке.
-   */
     login();
     driver.navigate().to("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");//открыть  админку на странице GeoZones
+
+    WebElement table = driver.findElement(By.className("dataTable"));//получить таблицу со страними
+    List<WebElement> rows = table.findElements(By.className("row"));//получить все строки таблицы
+    int size = rows.size();
+    for (int i = 0; i < size; i++)  {
+      WebElement row = rows.get(i);
+      WebElement cellWithName = row.findElement(By.xpath(".//td[3]"));
+      WebElement countryName = cellWithName.findElement(By.xpath("./a"));//найти ссылку по которой можно кликнуть
+      countryName.click();//открыть страницу страны
+
+
+      List<WebElement> zones = driver.findElements(By.xpath("//select[contains(@name, 'zones') and contains(@name, '[zone_code]')]"));//получить все меню с зонами со страницы
+      List<String> zoneNames = new ArrayList<>();//создать пустой список с названиями зон
+      for (WebElement zone : zones){
+        WebElement selectedZone = zone.findElement(By.xpath("./option[@selected='selected']"));//получить выбранную зону для данного выпадающего меню
+        String name = selectedZone.getText();
+        zoneNames.add(name);//заполнить список выбранных зон конкретными названиями
+      }
+
+
+      List<String> sortedZones = new ArrayList<>(zoneNames);
+      Collections.sort(sortedZones);
+      List<String> sortedZonesResult = new ArrayList<>(sortedZones);//отсортировать полученные со страницы зоны по алфавиту и записать это в список
+
+      assertEquals(sortedZonesResult, zoneNames);//проверить, что отсортированный список зон соответствует списку зон с сайта
+
+      driver.navigate().to("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+      table = driver.findElement(By.className("dataTable"));
+      rows = table.findElements(By.className("row"));
+    }
 
   }
 }
